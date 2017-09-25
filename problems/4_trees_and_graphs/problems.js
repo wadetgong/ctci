62be9193f9d3a6c7393446cb640aaf32193c0e60
Wade Gong <wadetgong@gmail.com>
2017-09-25 18:52:54 -0500

    completes exercises through 4.8

--- problems/4_trees_and_graphs/problems.js
+++ problems/4_trees_and_graphs/problems.js
@@ -1,10 +1,11 @@
 const BinarySearchTree = require('./BinarySearchTree')
 const { LinkedList } = require('../2_linked_lists/LinkedList')
+const { Graph, GraphNode } = require('./Graph')
 
 // 4.1 routeBetweenNodes
 const routeBetweenNodes = (nodeA, nodeB) => {
   let visited = new Map()
-  let visitQueue = [nodeA]
+  let visitQueue = [...nodeA.children]
   let currentNode
 
   while (visitQueue.length) {
@@ -62,6 +63,111 @@ const listOfDepths = (bst) => {
 
 // 4.4 checkBalanced
 const checkBalanced = bst => {
+  if (!bst) return true
+  let leftDepth = bst.left && bst.left.depth() || 0
+  let rightDepth = bst.right && bst.right.depth() || 0
+  return Math.abs(leftDepth - rightDepth) < 2 && checkBalanced(bst.left) && checkBalanced(bst.right)
+}
+
+// 4.5 validateBST
+const validateBST = binaryTree => {
+  if (!binaryTree) return true
+  let leftVal = binaryTree.left && binaryTree.left.value
+  let rightVal = binaryTree.right && binaryTree.right.value
+  let nodeVal = binaryTree.value
+  let leftChildCheck = leftVal && leftVal < nodeVal || true
+  let rightChildCheck = rightVal && rightVal > nodeVal || true
+  return leftChildCheck
+    && rightChildCheck
+    && validateBST(binaryTree.left)
+    && validateBST(binaryTree.right)
+}
+
+// 4.6 successor
+const successor = node => {
+  let currentNode = node
+  if (node.right) {
+    currentNode = node.right
+    while (currentNode.left) currentNode = currentNode.left
+    return currentNode
+  }
+  else {
+    currentNode = currentNode.parent
+    while (currentNode) {
+      if (currentNode.value >= node.value) return currentNode
+      currentNode = currentNode.parent
+    }
+    return null
+  }
+}
+
+// 4.7 buildOrder
+const makeBuild = (node, buildArr) => {
+  if (!node.children.length) {
+    if (!buildArr.includes(node.value)) {
+      buildArr.push(node.value)
+    }
+  } else {
+    for (let child of node.children) {
+      makeBuild(child, buildArr)
+      if (!buildArr.includes(node.value)) {
+        buildArr.push(node.value)
+      }
+    }
+  }
+  return buildArr
+}
+
+const buildOrder = (projects, dependencies) => {
+  let nodeHash = {}
+  projects.forEach(project => nodeHash[project] = new GraphNode(project))
+  dependencies.forEach(([dependee, depender]) => nodeHash[depender].add(nodeHash[dependee]))
+  for (let key in nodeHash) {
+    if (routeBetweenNodes(nodeHash[key], nodeHash[key])) return null
+  }
+
+  let build = []
+  for (let child in nodeHash) {
+    makeBuild(nodeHash[child], build)
+  }
+  return build
+}
+
+// 4.8 firstCommonAncestor
+const contains = (parentNode, childNode) => {
+  if (!parentNode) return false
+  if (parentNode === childNode) return true
+  let leftSearch = parentNode.left && contains(parentNode.left, childNode) || false
+  let rightSearch = parentNode.right && contains(parentNode.right, childNode) || false
+  return leftSearch || rightSearch
+}
+
+const firstCommonAncestor = (node1, node2, rootNode) => {
+  let currentNode = rootNode
+  let leftContains = contains(currentNode.left, node1) && contains(currentNode.left, node2)
+  let rightContains = contains(currentNode.right, node1) && contains(currentNode.right, node2)
+  while (leftContains || rightContains) {
+    if (leftContains) currentNode = currentNode.left
+    else currentNode = currentNode.right
+    leftContains = contains(currentNode.left, node1) && contains(currentNode.left, node2)
+    rightContains = contains(currentNode.right, node1) && contains(currentNode.right, node2)
+  }
+  return currentNode
+}
+
+// 4.9 bstSequences
+const bstSequences = bst => {
+  let perms = [bst.value]
+  let newPerms = []
+  let leftPerms = bstSequences(bst.left)
+  let rightPerms = bstSequences(bst.right)
+  for (let i = 0; i < leftPerms.length; i++) {
+    for (let j = 0; j < rightPerms.length; j++) {
+      newPerms.push([bst.value, ...leftPerms[i], ...rightPerms[j]])
+      newPerms.push([bst.value, ...rightPerms[j], ...leftPerms[i]])
+    }
+  }
+  return newPerms
 
 }
 
@@ -70,4 +176,9 @@ module.exports = {
   minimalTree,
   listOfDepths,
   checkBalanced,
+  validateBST,
+  successor,
+  buildOrder,
+  firstCommonAncestor,
+  bstSequences,
 }
--- problems/4_trees_and_graphs/specs.js
+++ problems/4_trees_and_graphs/specs.js
@@ -8,6 +8,11 @@ const {
   minimalTree,
   listOfDepths,
   checkBalanced,
+  validateBST,
+  successor,
+  buildOrder,
+  firstCommonAncestor,
+  bstSequences,
 } = require('./problems')
 
 describe('Chapter 4: Trees and Graphs -', () => {
@@ -84,6 +89,11 @@ describe('Chapter 4: Trees and Graphs -', () => {
       expect(routeBetweenNodes(node6, node2)).to.be.equal(false)
       expect(routeBetweenNodes(node7, node1)).to.be.equal(false)
     })
+
+    it('is able to check for cyclical routes', () => {
+      expect(routeBetweenNodes(node3, node3)).to.be.equal(false)
+      expect(routeBetweenNodes(node5, node5)).to.be.equal(true)
+    })
   })
 
   // 4.2 minimalTree
@@ -138,16 +148,235 @@ describe('Chapter 4: Trees and Graphs -', () => {
 
   // 4.4 checkBalanced
   describe('checkBalanced', () => {
-    it('returns returns true for a tree that is balanced', () => {
+    let node1, node2, node3, node4, node5, node6, node7, node8
+    beforeEach(() => {
+      node1 = new BinaryTree(1)
+      node2 = new BinaryTree(2)
+      node3 = new BinaryTree(3)
+      node4 = new BinaryTree(4)
+      node5 = new BinaryTree(5)
+      node6 = new BinaryTree(6)
+      node7 = new BinaryTree(7)
+      node8 = new BinaryTree(8)
+    })
 
+    it('returns returns true for a tree that is balanced', () => {
+      node1.left = node2
+      node1.right = node3
+      node2.left = node4
+      node2.right = node5
+      node3.left = node6
+      node3.right = node7
+      expect(checkBalanced(node1)).to.be.equal(true)
+      node4.left = node8
+      expect(checkBalanced(node1)).to.be.equal(true)
     })
 
     it('returns returns false for a tree that is not balanced', () => {
+      node1.left = node2
+      node1.right = node3
+      node2.left = node4
+      node2.right = node5
+      expect(checkBalanced(node1)).to.be.equal(true)
+      node4.left = node6
+      expect(checkBalanced(node1)).to.be.equal(false)
+      node3.right = node7
+      expect(checkBalanced(node1)).to.be.equal(true)
+    })
+  })
 
+  // 4.5 validateBST
+  describe('validateBST', () => {
+    let node1, node2, node3, node4, node5, node6, node7, node8
+    beforeEach(() => {
+      node1 = new BinaryTree(1)
+      node2 = new BinaryTree(2)
+      node3 = new BinaryTree(3)
+      node4 = new BinaryTree(4)
+      node5 = new BinaryTree(5)
+      node6 = new BinaryTree(6)
+      node7 = new BinaryTree(7)
+      node8 = new BinaryTree(8)
     })
 
-    it('returns returns false if for any given node in the tree, the height of any node\'s left child and the height of any node\'s right child differ by more than 1', () => {
+    it('returns true if a binary tree is a binary search tree', () => {
+      node5.left = node3
+      node5.right = node7
+      node3.left = node2
+      node3.right = node4
+      node2.left = node1
+      node7.left = node6
+      node7.right = node8
+      expect(validateBST(node5)).to.be.equal(true)
+      expect(validateBST(node3)).to.be.equal(true)
+      expect(validateBST(node7)).to.be.equal(true)
+    })
 
+    it('returns true if a binary tree is a binary search tree, even if unbalanced', () => {
+      node7.left = node4
+      node7.right = node8
+      node4.left = node3
+      node4.right = node5
+      node3.left = node2
+      node2.left = node1
+      node5.right = node6
+      expect(validateBST(node7)).to.be.equal(true)
+      expect(validateBST(node4)).to.be.equal(true)
+      expect(validateBST(node3)).to.be.equal(true)
+    })
+
+    it('returns false if a binary tree is not a binary search tree', () => {
+      node7.left = node4
+      node7.right = node8
+      node4.left = node3
+      node4.right = node5
+      node3.left = node2
+      node2.right = node1
+      node5.right = node6
+      expect(validateBST(node7)).to.be.equal(true)
+      expect(validateBST(node4)).to.be.equal(true)
+      expect(validateBST(node3)).to.be.equal(true)
+    })
+  })
+
+  // 4.6 successor
+  describe('successor', () => {
+    let node1, node2, node3, node4, node5, node6, node7, node8, node9, node10, node11, node12, node13, node14
+    beforeEach(() => {
+      node1 = new BinarySearchTree(1)
+      node2 = new BinarySearchTree(2)
+      node3 = new BinarySearchTree(3)
+      node4 = new BinarySearchTree(4)
+      node5 = new BinarySearchTree(5)
+      node6 = new BinarySearchTree(6)
+      node7 = new BinarySearchTree(7)
+      node8 = new BinarySearchTree(8)
+      node9 = new BinarySearchTree(9)
+      node10 = new BinarySearchTree(10)
+      node11 = new BinarySearchTree(11)
+      node12 = new BinarySearchTree(12)
+      node13 = new BinarySearchTree(13)
+      node14 = new BinarySearchTree(14)
+      node8.left = node4
+      node8.right = node12
+      node4.parent = node8
+      node12.parent = node8
+      node4.left = node2
+      node4.right = node6
+      node2.parent = node4
+      node6.parent = node4
+      node2.left = node1
+      node2.right = node3
+      node1.parent = node2
+      node3.parent = node2
+      node6.left = node5
+      node6.right = node7
+      node5.parent = node6
+      node7.parent = node6
+      node12.left = node10
+      node12.right = node14
+      node10.parent = node12
+      node14.parent = node12
+      node14.left = node13
+      node13.parent = node14
+      node10.left = node9
+      node10.right = node11
+      node9.parent = node10
+      node11.parent = node10
+    })
+
+    it('returns the next in-order node in a binary search tree', () => {
+      expect(successor(node1, node8)).to.be.equal(node2)
+      expect(successor(node2, node8)).to.be.equal(node3)
+      expect(successor(node3, node8)).to.be.equal(node4)
+      expect(successor(node4, node8)).to.be.equal(node5)
+      expect(successor(node5, node8)).to.be.equal(node6)
+      expect(successor(node6, node8)).to.be.equal(node7)
+      expect(successor(node7, node8)).to.be.equal(node8)
+      expect(successor(node8, node8)).to.be.equal(node9)
+      expect(successor(node9, node8)).to.be.equal(node10)
+      expect(successor(node10, node8)).to.be.equal(node11)
+      expect(successor(node11, node8)).to.be.equal(node12)
+      expect(successor(node12, node8)).to.be.equal(node13)
+      expect(successor(node13, node8)).to.be.equal(node14)
+    })
+
+    it('returns null if there is not a valid next node', () => {
+      expect(successor(node14, node8)).to.be.equal(null)
+    })
+  })
+
+  // 4.7 buildOrder
+  describe('buildOrder', () => {
+    it('returns a valid build order given a list of projects and dependencies, if one exists', () => {
+      let projects = ['a', 'b', 'c', 'd', 'e', 'f']
+      let dependencies = [
+        ['a', 'd'],
+        ['f', 'b'],
+        ['b', 'd'],
+        ['f', 'a'],
+        ['d', 'c'],
+      ]
+      let build = buildOrder(projects, dependencies)
+      expect(build).to.be.an.instanceof(Array)
+      expect(build.indexOf('a')).to.be.below(build.indexOf('d'))
+      expect(build.indexOf('f')).to.be.below(build.indexOf('b'))
+      expect(build.indexOf('b')).to.be.below(build.indexOf('d'))
+      expect(build.indexOf('f')).to.be.below(build.indexOf('a'))
+      expect(build.indexOf('d')).to.be.below(build.indexOf('c'))
+    })
+
+    it('returns null if a given list of projects and dependencies cannot lead to a valid build order', () => {
+      let projects = ['a', 'b', 'c', 'd']
+      let dependencies = [
+        ['a', 'b'],
+        ['b', 'c'],
+        ['c', 'd'],
+        ['d', 'a'],
+      ]
+      expect(buildOrder(projects, dependencies)).to.be.equal(null)
+    })
+  })
+
+  // 4.8 firstCommonAncestor
+  describe('firstCommonAncestor', () => {
+    let node1, node2, node3, node4, node5, node6, node7
+    beforeEach(() => {
+      node1 = new BinaryTree(1)
+      node2 = new BinaryTree(2)
+      node3 = new BinaryTree(3)
+      node4 = new BinaryTree(4)
+      node5 = new BinaryTree(5)
+      node6 = new BinaryTree(6)
+      node7 = new BinaryTree(7)
+      node1.left = node2
+      node1.right = node3
+      node2.left = node4
+      node2.right = node5
+      node3.left = node6
+      node3.right = node7
+    })
+
+    it('returns the first common ancestor of two nodes in a binary tree', () => {
+      expect(firstCommonAncestor(node6, node7, node1)).to.be.equal(node3)
+      expect(firstCommonAncestor(node2, node7, node1)).to.be.equal(node1)
+      expect(firstCommonAncestor(node3, node7, node1)).to.be.equal(node3)
+      expect(firstCommonAncestor(node2, node4, node1)).to.be.equal(node2)
+      expect(firstCommonAncestor(node5, node4, node1)).to.be.equal(node2)
+    })
+  })
+
+  // 4.9 bstSequences
+  describe('bstSequences', () => {
+    let node1, node2, node3
+    it('provides an array of arrays of valid insert seqeuences to produce the given binary search tree', () => {
+      node1 = new BinarySearchTree(1)
+      node2 = new BinarySearchTree(2)
+      node3 = new BinarySearchTree(3)
+      node2.left = node1
+      node2.right = node3
+      expect(bstSequences(node2)).to.be.an.instanceof(Array)
+      expect(bstSequences(node2).length).to.be.equal(2)
     })
   })
 })

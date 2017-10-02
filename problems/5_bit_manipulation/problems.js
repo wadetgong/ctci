e0ce4bc43db81c74267e2d3038fb8d1ca6595205
Wade Gong <wadetgong@gmail.com>
2017-10-01 22:20:55 -0500

    completes 5.1

--- problems/5_bit_manipulation/problems.js
+++ problems/5_bit_manipulation/problems.js
@@ -1,3 +1,22 @@
-module.exports = {
+const printBinary = (base10) => {
+  return parseInt(base10.toString(2))
+}
 
+const insertion = (N, M, i, j) => {
+  let NbinVal = parseInt(N.toString(), 2)
+  let MbinVal = parseInt(M.toString(), 2)
+
+  //Replace the relevant bit locations in N with 0's
+  let nClear = ~((1 << (j + 1)) - 1) + ((1 << i) - 1)
+  let clearedN = NbinVal & nClear
+
+  //Add necessary count of 0's to M
+  let shiftedM = MbinVal << i
+
+  //Combine clearedN with shiftedM via OR operation
+  return printBinary(clearedN | shiftedM)
+}
+
+module.exports = {
+  insertion,
 }
--- problems/5_bit_manipulation/specs.js
+++ problems/5_bit_manipulation/specs.js
@@ -1,5 +1,27 @@
 const { expect } = require('chai')
+const {
+  insertion,
+} = require('./problems')
 
 describe('Chapter 5: Bit Manipulation -', () => {
+  describe('insertion', () => {
+    it('inserts bits M into bits N starting at bit j and ending at bit i', () => {
+      let N = 10000000000
+      let M = 10011
+      let i = 2
+      let j = 6
+      expect(insertion(N, M, i, j)).to.be.equal(10001001100)
+      i = 1
+      j = 5
+      expect(insertion(N, M, i, j)).to.be.equal(10000100110)
+    })
 
+    it('overwrites bit locations in N with the respective M values', () => {
+      let N = 11111111111
+      let M = 10011
+      let i = 2
+      let j = 6
+      expect(insertion(N, M, i, j)).to.be.equal(11111001111)
+    })
+  })
 })
